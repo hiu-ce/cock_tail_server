bc1fe112922b3c152cbfd6974b86c53feeaec08b
kimsh1017 <kimsh8580@gmail.com>
2022-06-28 22:25:46 +0900

    remove base_settings

--- cocktail_server/cocktail_server/settings.py
+++ cocktail_server/cocktail_server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -87,12 +88,10 @@ DATABASES = {
         'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': 'hiuce12!@',
-        'HOST': '',
-        'PORT': ''
+        'HOST': '18.237.240.232',
+        'PORT': '3306'
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
